0d7db8246426ef850a43ff396d50bc7258fe0ad3
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-01 20:39:44 +0100

    Added static vs non-static examples

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.StaticVsNonStatic.Person;
+
 /**
  * Hello world!
  *
@@ -7,5 +9,28 @@ package org.example;
 public class App 
 {
     public static void main( String[] args ) {
+        System.out.println(Person.getAllPersons().size());
+        Person james = new Person("James", 22, "Warsaw");
+        System.out.println(Person.getAllPersons().size());
+        Person john = new Person("John", 40, "New York");
+        System.out.println(Person.getAllPersons().size());
+
+        // accessing non-static fields on different objects
+        System.out.println(james.getName());
+        System.out.println(john.getName());
+        System.out.println(james.getAge());
+        System.out.println(john.getCity());
+
+        // invoking non-static methods on different objects
+        james.introduce();
+        john.introduce();
+
+        // invoking static method via class
+        Person.helloWorld();
+
+        // invoking non-static methods on objects, while iterating on a static collection
+        for (Person person : Person.getAllPersons()) {
+            person.introduce();
+        }
     }
 }
