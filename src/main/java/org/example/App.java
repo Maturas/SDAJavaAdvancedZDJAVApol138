d927296551a57d8b39296ee497839043d45fa7fd
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-01 18:13:23 +0100

    Added Enum example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
+import org.example.EnumExample.Product;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+        Product myProduct = Product.BACON;
+
+        if (myProduct.equals(Product.SAUSAGE)) {
+            System.out.println("This is a sausage!");
+        }
+        else {
+            System.out.println("This is not a sausage!");
+        }
+
+        System.out.println(myProduct.getPrice());
+        System.out.println(myProduct.getVatRate());
     }
 }
