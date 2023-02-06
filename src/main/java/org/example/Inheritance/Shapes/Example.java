3206d4284bb4152eaa40d8b903a52167b16b8d8f
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-06 20:15:29 +0100

    Added abstract class example

--- src/main/java/org/example/Inheritance/Shapes/Example.java
+++ src/main/java/org/example/Inheritance/Shapes/Example.java
@@ -1,30 +1,26 @@
 package org.example.Inheritance.Shapes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Example {
     public static void example() {
-        Shape shape = new Shape();
-        Circle circle = new Circle(7.0);
-        Rectangle rectangle = new Rectangle(5.0, 4.0);
-        Square square = new Square(3.0);
+        // variable definition - constructor invocation
+        List<Shape> shapes = new ArrayList<>();
+        // constructor invocation as a parameter
+        // object gets constructed, and then is immediately passed as a parameter
 
-        System.out.println(shape.getName());
-        System.out.println(shape.getArea());
-        System.out.println(shape.getCircumference());
-        System.out.println();
+        // example of polymorphism - object has an actual type of Circle,
+        // but we perceive it as Shape, so that we can add it to a list of Shapes
+        shapes.add(new Circle(7.0));
+        shapes.add(new Rectangle(5.0, 4.0));
+        shapes.add(new Square(3.0));
 
-        System.out.println(circle.getName());
-        System.out.println(circle.getArea());
-        System.out.println(circle.getCircumference());
-        System.out.println();
-
-        System.out.println(rectangle.getName());
-        System.out.println(rectangle.getArea());
-        System.out.println(rectangle.getCircumference());
-        System.out.println();
-
-        System.out.println(square.getName());
-        System.out.println(square.getArea());
-        System.out.println(square.getCircumference());
-        System.out.println();
+        for (Shape shape : shapes) {
+            System.out.println(shape.getName());
+            System.out.println("Circumference: " + shape.getCircumference());
+            System.out.println("Area: " + shape.getArea());
+            System.out.println();
+        }
     }
 }
--- src/main/java/org/example/Inheritance/Shapes/Shape.java
+++ src/main/java/org/example/Inheritance/Shapes/Shape.java
@@ -2,16 +2,10 @@ package org.example.Inheritance.Shapes;
 
 // Shape - a general base class for all shapes
 // Defines, that all shapes have a name, a circumference and an area
-public class Shape {
-    public String getName() {
-        return "";
-    }
-
-    public Double getCircumference() {
-        return 0.0;
-    }
-
-    public Double getArea() {
-        return 0.0;
-    }
+// Shape is an abstract class; it cannot be instantiated (have objects)
+// instead, it serves as a base/template for its concrete (non-abstract) subclasses
+public abstract class Shape {
+    public abstract String getName();
+    public abstract Double getCircumference();
+    public abstract Double getArea();
 }
