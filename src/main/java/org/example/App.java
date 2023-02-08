3fc65b6cf4be971ee0596483567a16ae76585b49
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-08 20:38:12 +0100

    Added custom exception example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,8 @@
 package org.example;
 
-import org.example.DataAccessObject.AppLogic;
-import org.example.DataAccessObject.FakeClientDAO;
-import org.example.GenericTypes.NonGenericSafe;
-
 public class App
 {
     public static void main( String[] args ) {
-        FakeClientDAO dao = new FakeClientDAO();
-        AppLogic app = new AppLogic(dao);
+
     }
 }
