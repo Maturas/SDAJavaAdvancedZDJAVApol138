9196edf8179e1ab88f29fa5ded1a6a2c10f713fd
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-22 18:22:06 +0100

    Added basic multithreading example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,20 +1,10 @@
 package org.example;
 
-import org.example.Reflection.Book;
-import org.example.Reflection.ReflectionExamples;
+import org.example.MultiThreading.MultiThreadFizzBuzz;
 
 public class App
 {
     public static void main( String[] args ) {
-        Book hp = new Book("Harry Potter", "JK Rowling", 400);
-        ReflectionExamples.printAllFieldsWithCurrentValues(hp);
-        ReflectionExamples.changeFieldValue(hp, "title", "Chronicles of Narnia");
-        ReflectionExamples.changeFieldValue(hp, "author", "C.S. Lewis");
-        ReflectionExamples.printAllFieldsWithCurrentValues(hp);
-        ReflectionExamples.invokeMethod(hp, "printInfo");
-        ReflectionExamples.invokeMethodWithReturnValue(hp, "getTitle");
-        ReflectionExamples.invokeMethodWithParametersAndReturnValue(hp, "someMethodWithParameters",
-                new Object[] { "Hello!", 1000, "World!", 55.45 });
-        ReflectionExamples.createNewInstanceOfClass(Book.class, new Object[] { "The Witcher", "Andrzej Sapkowski", 300 });
+        MultiThreadFizzBuzz.example();
     }
 }
