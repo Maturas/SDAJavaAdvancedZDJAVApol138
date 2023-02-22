36145fc19cc6e5796db739151f70b63ab63f7bb2
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-22 18:53:24 +0100

    Added more mutithreading examples

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import org.example.MultiThreading.MultiThreadFizzBuzz;
+import org.example.MultiThreading.DeadlockExample;
 
 public class App
 {
     public static void main( String[] args ) {
-        MultiThreadFizzBuzz.example();
+        DeadlockExample.example();
     }
 }
